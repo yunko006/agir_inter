5c9d412145129a729b79f13dd59c7af82e013d67
yunko006 <peyrachethomas@gmail.com>
2023-01-25 23:52:34 +0100

    Update text routes and htmls

--- app/search/routes.py
+++ app/search/routes.py
@@ -1,12 +1,17 @@
 from flask import render_template, request
 
 from app.search import bp
+from app.models.intervenants import Intervenants
 
 
 @bp.route('/text', methods=['GET', 'POST'])
 def text():
     if request.method == 'POST':
-        get_text = request.form['text']
-        print(get_text)
-    
+        search = request.form['text']
+
+        result_search = Intervenants.objects.search_text(search).order_by('$text_score')
+
+        return render_template('search/text-results.html', results=result_search)
+
+
     return render_template('search/text.html')
