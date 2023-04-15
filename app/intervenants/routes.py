32ab5d909ac5624fe5778952dcfd88084365d411
yunko006 <peyrachethomas@gmail.com>
2023-04-16 00:45:24 +0200

    Create route and html for intervenants bp

--- app/intervenants/routes.py
+++ app/intervenants/routes.py
@@ -4,8 +4,22 @@ from app.intervenants import bp
 from app.extensions import db
 from app.models.intervenants import Intervenants
 
-@bp.route('/index')
-def index():
-    intervs = Intervenants.objects()
 
-    return render_template('intervenants/index.html', intervs=intervs)
+@bp.route('/delegation')
+def accordeon_delegation():
+    # get all distinct delegation to loop throught
+    delegation = Intervenants.objects.distinct('delegation')
+    benevoles = Intervenants.objects()
+
+    return render_template('intervenants/delegation.html', title='delegation', delegation=delegation, benevoles=benevoles)
+
+
+@bp.route('/<id>')
+def intervenant_par_id(id):
+        benevole_par_id = Intervenants.objects(id=id)
+        # get l'oject precis pour pouvoir avoir faire .id
+        int_benevole_id = Intervenants.objects.get(id=id)
+        # converti un int en str pour faire marcher dans le template html.
+        benevole_id = str(int_benevole_id.id)
+
+        return render_template('intervenants/intervenant_par_id.html',test=benevole_par_id, intervenant=benevole_id)
