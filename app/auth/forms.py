8309adf5c7295624de1d5100f37ff0a441b64e34
yunko006 <peyrachethomas@gmail.com>
2023-04-17 00:21:20 +0200

    Create forms and utils for auth routes

---
+++ app/auth/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Identifiant', validators=[DataRequired()])
+    password = PasswordField('Mot de passe', validators=[DataRequired()])
+    remember_me = BooleanField('Se souvenir de moi')
+    submit = SubmitField('Se connecter')
