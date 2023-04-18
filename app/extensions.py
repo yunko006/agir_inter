e00a19fa164fd73ff8bffcf55bef735cac50a679
yunko006 <peyrachethomas@gmail.com>
2023-04-18 21:12:02 +0200

    Add flask login and bcrypt to the project

--- app/extensions.py
+++ app/extensions.py
@@ -1,2 +1,11 @@
 from flask_mongoengine import MongoEngine
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from roles_required import not_ROLE
+
 db = MongoEngine()
+# intialize bcrypt
+bcrypt = Bcrypt()
+
+LoginManager.not_ROLE = not_ROLE
+login_manager = LoginManager()
