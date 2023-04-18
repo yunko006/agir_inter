62d6c7ab0aee4f2af52e708b4085eb3db4490a94
yunko006 <peyrachethomas@gmail.com>
2023-04-18 21:12:09 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,37 +1,54 @@
+from flask import Flask, redirect, render_template, url_for
 from flask_bootstrap import Bootstrap5
-from flask import Flask, render_template
-
+from flask_login import LoginManager
 
 from config import Config
 
-from .extensions import db
+from .extensions import db, login_manager
 
 
 def create_app(config_class=Config):
-    app=Flask(__name__)
+    app = Flask(__name__)
     app.config.from_object(config_class)
-    
+
     # initialize mongodb
     db.init_app(app)
 
     bootstrap = Bootstrap5(app)
-    
+
+    # login manager
+    # flask login set up
+    login_manager.init_app(app)
+    login_manager.login_view = "login"
+    login_manager.not_ROLE_view = "not_ROLE"
+
     # blueprints
     from app.intervenants import bp as intervenants_bp
+
     app.register_blueprint(intervenants_bp)
 
     from app.search import bp as search_bp
+
     app.register_blueprint(search_bp)
 
     from app.auth import auth as auth_bp
+
     app.register_blueprint(auth_bp)
 
-
-    @app.route('/test')
+    @app.route("/test")
     def test_page():
         return "test page"
 
+    @login_manager.unauthorized_handler
+    def unauthorized_callback():
+        return redirect(url_for("auth.login"))
+
+    @app.route("/not-ROLE/")
+    def not_ROLE():
+        return render_template("not_role.html")
+
     return app
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     create_app()
