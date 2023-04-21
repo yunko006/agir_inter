879f29416ca430c56c86fc343094a3eb83428e3a
yunko006 <peyrachethomas@gmail.com>
2023-04-21 12:48:59 +0200

    Create a function to convert data

--- app/models/intervenants.py
+++ app/models/intervenants.py
@@ -1,7 +1,8 @@
 import mongoengine as me
 
+
 class Intervenants(me.Document):
-    meta = {'collection': 'intervenants', 'strict': False}
+    meta = {"collection": "intervenants", "strict": False}
     # mapping from db
     email = me.StringField()
     nom = me.StringField()
@@ -21,9 +22,9 @@ class Intervenants(me.Document):
     domaines = me.StringField()
     fonctions = me.StringField()
     competences = me.StringField()
-    complements =me.StringField()
+    complements = me.StringField()
     dispo = me.StringField()
-    duree =  me.StringField()
+    duree = me.StringField()
     deplacements_par_an = me.StringField()
     langue_maternelle = me.StringField()
     notions = me.StringField()
@@ -38,6 +39,49 @@ class Intervenants(me.Document):
     connaissances = me.StringField()
     connaissances_structure = me.StringField()
     delegation = me.StringField()
-    idees= me.StringField()
+    idees = me.StringField()
     linkedin = me.StringField()
     autres_langues = me.StringField()
+
+    def to_dict(self):
+        return {
+            "id": str(self.id),
+            "nom": self.nom,
+            "prenom": self.prenom,
+            "email": self.email,
+            "volontaire": self.volontaire,
+            "hesitations": self.hesitations,
+            "contact": self.contact,
+            "aider": self.aider,
+            "voyager": self.voyager,
+            "travailler_en_equipe": self.travailler_en_equipe,
+            "soutenir": self.soutenir,
+            "connaitre_personnes_cultures": self.connaitre_personnes_cultures,
+            "mettre_a_dispotion": self.mettre_a_dispotion,
+            "responsabilites": self.responsabilites,
+            "actif": self.actif,
+            "autres_facteurs": self.autres_facteurs,
+            "domaines": self.domaines,
+            "fonctions": self.fonctions,
+            "competences": self.competences,
+            "complements": self.complements,
+            "dispo": self.dispo,
+            "duree": self.duree,
+            "deplacements_par_an": self.deplacements_par_an,
+            "langue_maternelle": self.langue_maternelle,
+            "notions": self.notions,
+            "lu_parle_ecrit": self.lu_parle_ecrit,
+            "autonomes": self.autonomes,
+            "roles": self.roles,
+            "exp_inter_oui_non": self.exp_inter_oui_non,
+            "exp_pro": self.exp_pro,
+            "exp_benevole": self.exp_benevole,
+            "structures": self.structures,
+            "suggestions": self.suggestions,
+            "connaissances": self.connaissances,
+            "connaissances_structure": self.connaissances_structure,
+            "delegation": self.delegation,
+            "idees": self.idees,
+            "linkedin": self.linkedin,
+            "autres_langues": self.autres_langues,
+        }
