44880079cabefa9ed70b90f693a35d9bbf0c843c
yunko006 <peyrachethomas@gmail.com>
2023-04-21 12:47:52 +0200

    Add flask_session to the project
    not used atm

--- app/extensions.py
+++ app/extensions.py
@@ -2,6 +2,7 @@ from flask_mongoengine import MongoEngine
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from roles_required import not_ROLE
+from flask_session import Session
 
 db = MongoEngine()
 # intialize bcrypt
@@ -9,3 +10,5 @@ bcrypt = Bcrypt()
 
 LoginManager.not_ROLE = not_ROLE
 login_manager = LoginManager()
+
+sess = Session()
