45112082bb1c6a3b1f1fbfe9e0dc8f4055c1652f
yunko006 <peyrachethomas@gmail.com>
2023-05-01 20:53:59 +0200

    Fix syntax formatting

--- app/intervenants/routes.py
+++ app/intervenants/routes.py
@@ -5,21 +5,30 @@ from app.extensions import db
 from app.models.intervenants import Intervenants
 
 
-@bp.route('/delegation')
+@bp.route("/delegation", methods=["GET", "POST"])
 def accordeon_delegation():
     # get all distinct delegation to loop throught
-    delegation = Intervenants.objects.distinct('delegation')
+    delegation = Intervenants.objects.distinct("delegation")
     benevoles = Intervenants.objects()
 
-    return render_template('intervenants/delegation.html', title='delegation', delegation=delegation, benevoles=benevoles)
+    return render_template(
+        "intervenants/delegation.html",
+        title="delegation",
+        delegation=delegation,
+        benevoles=benevoles,
+    )
 
 
-@bp.route('/<id>')
+@bp.route("/<id>")
 def intervenant_par_id(id):
-        benevole_par_id = Intervenants.objects(id=id)
-        # get l'oject precis pour pouvoir avoir faire .id
-        int_benevole_id = Intervenants.objects.get(id=id)
-        # converti un int en str pour faire marcher dans le template html.
-        benevole_id = str(int_benevole_id.id)
+    benevole_par_id = Intervenants.objects(id=id)
+    # get l'oject precis pour pouvoir avoir faire .id
+    int_benevole_id = Intervenants.objects.get(id=id)
+    # converti un int en str pour faire marcher dans le template html.
+    benevole_id = str(int_benevole_id.id)
 
-        return render_template('intervenants/intervenant_par_id.html',test=benevole_par_id, intervenant=benevole_id)
+    return render_template(
+        "intervenants/intervenant_par_id.html",
+        test=benevole_par_id,
+        intervenant=benevole_id,
+    )
