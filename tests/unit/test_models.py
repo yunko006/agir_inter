d2b5abee884dd9caeac84eee7d7a65c2c335785c
yunko006 <peyrachethomas@gmail.com>
2023-05-17 00:08:35 +0200

    Add: test_new_user

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,4 +1,23 @@
 from app.models.intervenants import Intervenants
+from app.auth.models import User
+
 
 def test_new_intervenant():
     pass
+
+
+def test_new_user():
+    user = User(
+        username="testuser", password="password", roles="Admin", delegation="Nice"
+    )
+
+    assert user.username == "testuser"
+    assert user.password == "password"
+    assert user.roles == "Admin"
+    assert user.delegation == "Nice"
+
+
+# def test_logged_in_test_user(logged_in_user):
+#     # Utilisez l'utilisateur connecté dans ce test
+#     assert logged_in_user.username == "testuser"
+#     assert logged_in_user.roles == "Admin"
