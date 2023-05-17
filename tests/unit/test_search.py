1674ace76813bd91e50bdc2ef6c5fd1b0aa9ba45
yunko006 <peyrachethomas@gmail.com>
2023-05-18 00:33:22 +0200

    Add: 2 tests for add field & combinaison

--- tests/unit/test_search.py
+++ tests/unit/test_search.py
@@ -34,3 +34,64 @@ def test_intervenants_view(test_client):
     assert b"Prenom" in response.data
 
     # Ajoutez ici la logique nécessaire pour vérifier le contenu de la réponse
+
+
+def test_add_field_view(test_client):
+    response = test_client.post("search/add_field")
+
+    # Vérifiez que la réponse a un code de statut 200
+    assert response.status_code == 200
+    # Vérifiez que le bon template est utilisé
+    assert b"Minus" in response.data
+
+
+def test_combinaison_search_view_with_data(test_client):
+    recherche_data = ["recherche1", "recherche2"]
+    champs_data = ["langue_maternelle", "domaines"]
+
+    response = test_client.post(
+        "search/combinaison_search",
+        data={"recherche": recherche_data, "champs": champs_data},
+    )
+
+    assert response.status_code == 200
+    # assert b"recherche_data" in response.data
+    # assert b"champs_data" in response.data
+    assert b"Nom" in response.data
+    # Effectuez des tests supplémentaires selon vos besoins
+
+
+def test_combinaison_search_view_without_data(test_client):
+    response = test_client.post("search/combinaison_search")
+
+    # Vérifiez que la réponse a un code de statut 200
+    assert response.status_code == 200
+    assert b"chargement des r" in response.data
+
+    # Effectuez des tests supplémentaires selon vos besoins
+
+
+# def test_combinaison_sur_txt_search_view_with_data(test_client):
+#     recherche_data = ["recherche1", "recherche2"]
+#     champs_data = ["langue_maternelle", "domaines"]
+
+#     response = test_client.post(
+#         "search/combinaison_resultats",
+#         data={"recherche": recherche_data, "champs": champs_data},
+#     )
+
+#     assert response.status_code == 200
+#     # assert b"recherche_data" in response.data
+#     # assert b"champs_data" in response.data
+#     assert b"Nom" in response.data
+#     # Effectuez des tests supplémentaires selon vos besoins
+
+
+# def test_combinaison_sur_txt_search_view_without_data(test_client):
+#     response = test_client.post("search/combinaison_resultats")
+
+#     # Vérifiez que la réponse a un code de statut 200
+#     assert response.status_code == 200
+#     assert b"chargement des r" in response.data
+
+#     # Effectuez des tests supplémentaires selon vos besoins
